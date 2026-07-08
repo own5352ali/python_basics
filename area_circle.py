e71ac93a463fc9f9d6d1c29af88c0da5adf8e9bd
Own Ali <own.5352.ali@gmail.com>
2026-07-08 12:40:34 +0500

    Used pow instead of two Radius multiplication

--- area_circle.py
+++ area_circle.py
@@ -2,6 +2,6 @@ import math
 
 Radius = float(input("Enter the Radius for Circle: "))
 
-Area = math.pi * Radius * Radius
+Area = math.pi * pow(Radius, 2)
 
 print(f"The Area of the Circle is: {Area}")
