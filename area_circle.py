d428d19a4dea89ab4314a4b3f23065c9fd7febd6
Own Ali <own.5352.ali@gmail.com>
2026-07-08 12:41:58 +0500

    Used Round off function to 1 decimal place

--- area_circle.py
+++ area_circle.py
@@ -4,4 +4,4 @@ Radius = float(input("Enter the Radius for Circle: "))
 
 Area = math.pi * pow(Radius, 2)
 
-print(f"The Area of the Circle is: {Area}")
+print(f"The Area of the Circle is: {round(Area, 1)}")
