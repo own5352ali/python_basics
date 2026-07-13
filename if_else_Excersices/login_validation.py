b118ea813577fa899de974aee250692864890536
Own Ali <own.5352.ali@gmail.com>
2026-07-13 17:30:27 +0500

    Fixed the bug in my condition statements

--- if_else_Excersices/login_validation.py
+++ if_else_Excersices/login_validation.py
@@ -6,11 +6,11 @@ password = input("Enter your Password ")
 if user_name == 'admin' and password == '12345':
      print("Login Sucessfull!")
 
+elif user_name != 'admin' and password != '12345':
+     print("Invalid Credentials!")
+
 elif user_name != 'admin':
      print("Invalid Username")
 
-elif password != '12345':
-     print("Invalid Password")
-
 else:
-     print("Invalid Credentials!")
+     print("Invalid Password")
