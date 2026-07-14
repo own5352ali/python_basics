9b0594090953ac359bb916c8cc02c43be10b12a4
Own Ali <own.5352.ali@gmail.com>
2026-07-14 11:42:49 +0500

    Modified my BMI Calculator

--- if_else_Excersices/BMI_Calculator.py
+++ if_else_Excersices/BMI_Calculator.py
@@ -3,21 +3,18 @@ Height = float(input("Enter your Height 'M': "))
 Weight = float(input("Enter your Weight in 'Kg': "))
 
 BMI = Weight / pow(Height, 2) 
+print(f"Your BMI is: {round(BMI,2)}")
 
 if BMI < 18.5:
-    print(f"{round(BMI,2)}")
     print("Underweight")
 
 elif BMI >= 18.5 and BMI <= 24.9:
-    print(f"{round(BMI,2)}")
     print("Normal Weight")
 
 elif BMI >= 25 and BMI <= 29.9:
-    print(f"{round(BMI,2)}")
     print("Overweight")
 
 else:
-    print(f"{round(BMI,2)}")
     print("Obese")
 
 
