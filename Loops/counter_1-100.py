8b7444ef5cb9d1e3227dc921d55784d5598a68f8
Own Ali <own.5352.ali@gmail.com>
2026-07-15 11:02:30 +0500

    It's my first basic counter using loops

---
+++ Loops/counter_1-100.py
@@ -0,0 +1,19 @@
+# Counter for Numbers till 100:
+for x in range (1, 101):
+    print(x, end=", ")
+print("\n")
+
+# Counter for Reversed Numbers:
+for x in reversed(range (1, 101)):
+    print(x, end=", ")
+print("\n")
+
+# Counter for Odd Numbers: 
+for x in range (1, 101, 2):
+    print(x, end=", ")
+print("\n")
+
+# Counter for Even Numbers:
+for x in range (2, 101, 2):
+    print(x, end=", ")
+print("\n")
