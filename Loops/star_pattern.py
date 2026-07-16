a15342805a8a79f75660e88505bc0ad1e51303bf
Own Ali <own.5352.ali@gmail.com>
2026-07-16 15:17:47 +0500

    Add Half diamond program

---
+++ Loops/star_pattern.py
@@ -0,0 +1,13 @@
+
+rows = int(input("Enter Number of Rows for Star: "))
+
+for row in range (1, rows+1):
+
+    for space in range(rows - row):
+        print(" ", end="")
+
+    for stars in range(2 * row - 1):
+            print("*", end="")
+
+    print()
+
