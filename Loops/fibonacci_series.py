2d299ffd2f6516c068f25b833056f59a59113efb
Own Ali <own.5352.ali@gmail.com>
2026-07-17 11:37:51 +0500

    Modified my code for Fibonacci Series

--- Loops/fibonacci_series.py
+++ Loops/fibonacci_series.py
@@ -3,7 +3,17 @@ num = int(input("Enter the number for Fibonacci Series: "))
 
 first_num = 0
 second_num = 1
-next = 0
+
+if num <= 0:
+    print("Please enter a positive number.")
+
+elif num == 1:
+    print(first_num)
+
+else:
+    print(first_num)
+    print(second_num)
+
 for i in range (1, num+1):
     next = first_num + second_num
     first_num = second_num
