0cbb73a5c52eeaf93fe7fcf02d10619e23ca7a28
Own Ali <own.5352.ali@gmail.com>
2026-08-20 13:30:36 +0500

    Add Encryption / Decryption program

---
+++ Encryption Decryption/program.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#Encryption
+
+plain_text = input("Enter a Message to Encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original Message: {plain_text}")
+print(f"Cipher Text: {cipher_text}")
+
+#Decryption
+
+cipher_text = input("Enter a Message to Decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Cipher Text: {cipher_text}")
+print(f"Original Message: {plain_text}")
+
+
+
+
+
